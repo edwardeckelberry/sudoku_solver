08a60e333e42997ed32f2788455098c91d6fc2ee
ed <edward.eckelberry@gmail.com>
2025-06-25 13:41:24 -0700

    made file to check correctness

---
+++ check.js
@@ -0,0 +1,5 @@
+//this is to check if the 2d arr is valid in sudoku
+export function isValidSudoku(board) {
+    
+
+}
--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-puzzle = "string";
+
+puzzle = "";
+
+//gets puzzle from txt file
 const fs = require('node:fs');
 try {
     const data = fs.readFileSync('read.txt', 'utf8');
+
+    //put puzzle in string
     puzzle = data;
 } catch (err) {
     console.error(err);
 }
 
-console.log(puzzle);
+//console.log(puzzle);
+
+const og = puzzle.map(row =>
+        row.map(str => Number(str)) // 10 specifies base 10 (decimal)
+    );
+
+//og = parseInt(puzzle);
+console.log(og);
