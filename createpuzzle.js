797c85c0201cae251d158a9f63a20cf5b0f19652
ed <edward.eckelberry@gmail.com>
2025-07-11 09:08:55 -0700

    made a function to check if valid

--- createpuzzle.js
+++ createpuzzle.js
@@ -3,8 +3,18 @@ export function createPuzzle(grid) {
         const row = [];
         for(let j = 0; j < 9; j++){
             row.push(Math.floor(Math.random() * 10));
+                
         }
         grid.push(row);
     }
     return grid;
+}
+
+export function checkPuzzle(grid, r, c, k) {
+    if(k != grid[r]){
+        for(let i = 0; i < 9; i++){
+            if(k != grid[i][c]) continue;
+        }
+        
+    }
 }
